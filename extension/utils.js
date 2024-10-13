de0dba0514077af1da3731299084a5e30cbf284e
Stearells <stearells@gmail.com>
2024-10-13 17:07:36 +0300

    added readme

---
+++ extension/utils.js
@@ -0,0 +1,13 @@
+// Stearells (C)
+// github.com/stearells
+// ыЫ :-)
+
+const res = chrome.runtime.getURL;
+
+/*const injectCSS = (css) => {
+    let e = document.createElement('style');
+    e.innerText = css;
+    document.head.append(e);
+
+    return e;
+}*/
